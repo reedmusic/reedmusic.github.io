a781b1eb5e81acc439fb4948a83d78f975843887
reedmusic <18534831+reedmusic@users.noreply.github.com>
2021-12-06 12:44:19 +0000

    Create template.rb

---
+++ y7/template.rb
@@ -0,0 +1,41 @@
+
+use_bpm 250
+use_synth :hoover
+
+# --- 1st bar ---
+
+sleep 0.5
+# 1
+
+
+sleep 0.5
+# +
+
+
+
+sleep 0.5
+# 2
+
+
+
+sleep 0.5
+# +
+
+
+sleep 0.5
+# 3
+
+
+sleep 0.5
+# +
+
+
+sleep 0.5
+# 4
+
+
+sleep 0.5
+# +
+
+
+#-----------
